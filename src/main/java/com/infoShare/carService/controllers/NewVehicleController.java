af8da309d00c70119ffacf95f3370ab697a24c75
lyingparachute <igor1501@o2.pl>
2022-03-04 13:10:58 +0100

    Validate POST method

--- src/main/java/com/infoShare/carService/controllers/NewVehicleController.java
+++ src/main/java/com/infoShare/carService/controllers/NewVehicleController.java
@@ -1,10 +1,10 @@
 package com.infoShare.carService.controllers;
 
 import com.infoShare.carService.dto.CreateVehicleDto;
-import com.infoShare.carService.dto.VehicleDto;
 import com.infoShare.carService.service.VehicleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,21 +13,25 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("vehicle")
-public class AddVehicleController {
+public class NewVehicleController {
     private final VehicleService vehicleService;
 
-    public AddVehicleController(VehicleService vehicleService) {
+    public NewVehicleController(VehicleService vehicleService) {
         this.vehicleService = vehicleService;
     }
 
     @GetMapping("new")
     public String getNewVehicleForm(Model model){
         model.addAttribute("vehicle", new CreateVehicleDto());
-        return "vehicle-form";
+        return "new-vehicle-form";
     }
 
     @PostMapping(value = "new")
-    public String createVehicle(@Valid @ModelAttribute("vehicle") CreateVehicleDto vehicle, Model model) {
+    public String createVehicle(@Valid @ModelAttribute("vehicle") CreateVehicleDto vehicle, Model model,
+                                Errors errors) {
+        if (errors.hasErrors()) {
+            return "new-vehicle-form";
+        }
         model.addAttribute("vehicle", vehicleService.saveVehicle(vehicle));
         return "redirect:/vehicles";
     }
